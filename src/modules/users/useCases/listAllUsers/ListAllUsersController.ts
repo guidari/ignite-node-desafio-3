71ace4ce4f59a1eead4ece6487544f55cd8e1013
Guilherme Datilio Ribeiro1 <guilherme.datilio@ibm.com>
2021-07-24 12:00:44 -0300

    Challange done

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -6,7 +6,17 @@ class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    try {
+      const { user_id } = request.headers;
+
+      const userList = this.listAllUsersUseCase.execute({
+        user_id: String(user_id),
+      });
+
+      return response.status(201).json(userList);
+    } catch (err) {
+      return response.status(400).json({ error: err });
+    }
   }
 }
 
